0da7667d66a9d159c8a7a1378e9aad599835502d
ysfss <61326539+ysfsvm@users.noreply.github.com>
2023-03-31 15:40:59 +0300

    Change ezan api to sabah.com.tr

--- scripts/ezan.py
+++ scripts/ezan.py
@@ -5,7 +5,7 @@
 # Created Date: 29.03.2023
 # license = MIT
 #----------------------------------------------------------------------------
-
+import sys
 try:
     import requests
     from datetime import datetime
@@ -18,42 +18,42 @@ except ImportError:
     print("pip install termcolor")
     print("Gerekirse 'pip install emoji' komutunu da girebilirsiniz.")
 
-city = input("Şehir: ")
+# Argüman olarak şehir adı verilmiş mi kontrol et
+if len(sys.argv) > 1:
+    city = sys.argv[1]
+else:
+    city = input("Lütfen şehir adını girin: ")
 
-# Method 13 = Diyanet İşleri Başkanlığı, Turkey
-api_url = f"http://api.aladhan.com/v1/timingsByCity?city={city}&country=Turkey&method=13"
+
+api_url = f"https://www.sabah.com.tr/json/getpraytimes/{city.lower()}"
 response = requests.get(api_url)
 
 if response.status_code == 200:
-    data = response.json()['data']
-    timings = data['timings']
+    data = response.json()['List'][0]
 
-    # Vakitlerin Türkçe karşılıkları ve emojileri
     vakitler = {
-        'Fajr': emoji.emojize(':new_moon: İmsak  '),
-        'Sunrise': emoji.emojize(':sunrise: Güneş  '),
-        'Dhuhr': emoji.emojize(':sun:  Öğle   ' ),
-        'Asr': emoji.emojize(':sunset: İkindi '),
-        'Maghrib': emoji.emojize(':bridge_at_night: Akşam  '),
-        'Isha': emoji.emojize(':night_with_stars: Yatsı  ')
+        'Imsak': emoji.emojize(':new_moon: İmsak  '),
+        'Gunes': emoji.emojize(':sunrise: Güneş  '),
+        'Ogle': emoji.emojize(':sun:  Öğle   ' ),
+        'Ikindi': emoji.emojize(':sunset: İkindi '),
+        'Aksam': emoji.emojize(':bridge_at_night: Akşam  '),
+        'Yatsi': emoji.emojize(':night_with_stars: Yatsı  ')
     }
 
-    # Tablo oluşturmak için hücre genişliklerini hesapla
     max_len = max([len(v) for v in vakitler.values()])
     cell_width = max(max_len, 5) + 2  # 5 karakterden kısa olmamalı
 
-    # Tablo başlığı
     table = f"\n{'-' * (cell_width * 2 - 4)}"
     table += f"\n|{' Vakit':^{cell_width-1}}|{'Saat':^{cell_width-6}}|\n"
     table += f"{'-' * (cell_width * 2 - 4)}\n"
 
-    # Vakitleri tabloya ekle
     for vakit, saate in vakitler.items():
-        time_str = datetime.strptime(timings[vakit], '%H:%M').strftime('%H:%M')
+        time_str = datetime.fromtimestamp(int(data[vakit][6:-2])/1000).strftime('%H:%M')
         table += f"| {colored(saate, 'yellow'):<{cell_width}} | {colored(time_str, 'green'):^{cell_width}} |\n"
     table += f"{'-' * (cell_width * 2 - 4)}"
     print(table)
-    print("| 💀 API:aladhan.com |")
+    print("| 💀 API:sabah.com.tr |")
     print("----------------------\n")
 else:
     print(colored("\033[31mHata: İstek yapılamadı.\033[0m", 'red'))
+
